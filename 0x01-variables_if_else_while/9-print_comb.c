65c7537465c57155638d0c58091ed48d1b3d5874
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-17 16:35:05 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,13 @@ int main(void)
 int i, j;
 for (i = 0; i < 10; i++)
 {
-for (j = i; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
+putchar(',');
+putchar(' ');
 putchar(j + '0');
-if (i != 9 || j != 9)
+if (i < 8 || j < 9)
 {
 putchar(',');
 putchar(' ');

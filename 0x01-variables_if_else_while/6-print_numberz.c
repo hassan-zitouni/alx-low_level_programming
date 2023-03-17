146ec6e53e46136eada5056d36e5f8d1fddfa730
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-17 16:15:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * This program generates a random number and prints whether it is positive,
+ * negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

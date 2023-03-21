3292a710a358291cc7ee8ab7cce6bd768814c84b
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-21 12:53:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 char ch;
 int i;
 i = 0;
-while (i <10)
+while (i < 10)
 {
-for (ch = 'a'; ch <= 'z'; ch++) {
+for (ch = 'a'; ch <= 'z'; ch++)
+{
 _putchar(ch);
 }
-
 _putchar('\n');
 i++;
 }

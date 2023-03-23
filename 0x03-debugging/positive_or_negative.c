ce26d1fa6058230721f61a5748828ac20f28cd87
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-22 17:05:26 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * This program generates a random number and prints whether it is positive,
+ * negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+
+	return;
+}

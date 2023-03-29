f39ab436864df4acab8a33e9c9427d7111080606
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-29 16:14:46 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,26 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i:
-i = 0;
+int i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
-return (s1[i] - s2[i]);
+return (s1[i] < s2[i] ? -1 : 1);
 }
-i++;
+i++
 }
+if (s1[i] == '\0' && s2[i] == '\0')
+{
 return (0);
 }
+else if (s1[i] == '\0')
+{
+return (-1);
+}
+else
+{
+return (1);
+}
+}
+

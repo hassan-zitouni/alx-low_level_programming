bcc19edf7b6aaccd8ed595728aa850956f4a01a6
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-29 17:56:12 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,12 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i])
 {
-while (!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z'))
+while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
-if (str[i - 1] == ',' ||
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
 str[i - 1] == ';' ||
 str[i - 1] == '.' ||
 str[i - 1] == '!' ||
@@ -22,13 +25,8 @@ str[i - 1] == '(' ||
 str[i - 1] == ')' ||
 str[i - 1] == '{' ||
 str[i - 1] == '}' ||
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' ||
 i == 0)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
-}
 i++;
 }
 return (str);

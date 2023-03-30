55d1bc2fe4d750057bb2e86ebb8c6ce2dc79f33f
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-03-29 17:15:53 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 int i = 0;
 while (str[i])
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'))
+while (!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z'))
 i++;
 if (str[i - 1] == ',' ||
 str[i - 1] == ';' ||
@@ -25,7 +25,10 @@ str[i - 1] == '}' ||
 str[i - 1] == '\t' ||
 str[i - 1] == '\n' ||
 i == 0)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= 32;
+}
 i++;
 }
 return (str);

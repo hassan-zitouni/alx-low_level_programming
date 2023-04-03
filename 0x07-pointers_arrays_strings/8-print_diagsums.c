9c7ff452b09b40d25257e0a2c46a0f154f553b8d
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-03 11:09:36 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagsums - a function that prints the sum of the two diagonals
+ * @a: pointed destination
+ * @size: input
+ * Return: always 0 (succes)
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum1 = 0, sum2 = 0;
+for (i = 0; i <= (size * size); i = i + size + 1)
+sum1 = sum1 + a[i];
+for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
+sum2 = sum2 + a[j];
+printf("%d, %d/n", sum1, sum2);
+}
+

3f1cba9a47f82f5649a46b995e2b3b18a70e454f
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-05 09:45:12 -0700

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * is_prime_number - a function input integer
+ * @n: input
+ * Return: always 0 (succes)
+ */
+int is_prime_number(int n)
+{
+int i;
+if (n <= 1)
+{
+return (0);
+}
+for (i = 2; i * i <= n; i++)
+{
+if (n % i == 0)
+{
+return (0);
+}
+}
+return (1);
+}

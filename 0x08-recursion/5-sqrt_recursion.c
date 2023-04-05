8c802cb53e2ea374c5e7ad8726621deea40fc9db
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-05 09:26:48 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int _sqrt_helper(int n, int start, int end);
+int _sqrt_helper(int n, int start);
 /**
  * _sqrt_recursion - a function that return natural square
  * @n: input
@@ -8,33 +8,20 @@ int _sqrt_helper(int n, int start, int end);
  */
 int _sqrt_recursion(int n)
 {
-return (_sqrt_helper(n, 0, n));
+return (_sqrt_helper(n, 1));
 }
 /**
  * _sqrt_helper - a function that return natural square
  * @n: input
- * @end: input
  * @start: input
  * Return: always 0 (succes)
  */
-int _sqrt_helper(int n, int start, int end)
+int _sqrt_helper(int n, int start)
 {
-int mid = 0;
-if (start > end)
-{
+int sqrt = start * start;
+if (sqrt > n)
 return (-1);
-}
-mid = (start + end) / 2;
-if (mid *mid == n)
-{
-return (mid);
-}
-else if (mid *mid > n)
-{
-return (_sqrt_helper(n, start, mid - 1));
-}
-else
-{
-return (_sqrt_helper(n, mid + 1, end));
-}
+if (sqrt == n)
+return (start);
+return (_sqrt_helper(n, start + 1);
 }

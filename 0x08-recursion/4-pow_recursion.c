68e127349e186a297dc36aedc82913c73930eebe
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-05 08:51:35 -0700

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that returns the value
  * @x: input
  * @y: input
  * Return: always 0 (succes)

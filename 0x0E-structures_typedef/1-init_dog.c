44877778a8a76c1625cbf2653af5cfb2e12df308
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-17 12:57:58 -0700

    0x0E-structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes dog
+ * @d: input
+ * @name: name
+ * @age: age
+ * @owner: owneri
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}

ad317ad8758b5f6d2ec240de4a4094c37a46d18b
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-17 12:44:02 -0700

    0x0E-structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,22 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 /**
- * init_dog - initialized struct_dog
+ * struct dog - initialized struct_dog
  * @d: input
  * @name: input
  * @age: input
  * @owner: input
- * Return: Always 0.
+ * Description: this struct is for dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+char* name;
+float age;
+char* owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */

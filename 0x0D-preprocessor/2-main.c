a938c89a161a9a5748a49a2bb233213fd8f18252
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-17 17:38:05 -0700

    0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("The name of this file is: %s\n", __FILE__);
+return (0);
+}

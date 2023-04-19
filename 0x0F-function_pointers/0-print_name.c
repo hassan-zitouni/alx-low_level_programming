f50c31f32364e0c86f6f950ed66a64bf38d3b507
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-19 08:15:02 -0700

    0-print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,14 @@
 
 /**
  * print_name - Write a function that prints a name
- * @name: input value
- * @f: input value
+ * @name: string to add
+ * @f: pointer
  * Return: nothing
- */
+ **/
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 f(name);
 }
-

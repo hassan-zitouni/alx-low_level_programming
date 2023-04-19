9772bc12bb509985079ebaf1706d509c071a5150
hassan-zitouni <hassan.zitouni@um5s.net.ma>
2023-04-19 09:03:35 -0700

    0-print_name

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - the sum
+ * @a: input value
+ * @b: input value
+ * Return: the sum
+ **/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - the difference
+ * @a: input value
+ * @b: input value
+ * Return: the difference
+ **/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - the product
+ * @a: input value
+ * @b: input value
+ * Return: the product
+ **/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - the division
+ * @a: input value
+ * @b: input value
+ * Return: the division
+ **/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - the module
+ * @a: input value
+ * @b: input value
+ * Return: the module
+ **/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
